a57f75dd781401498e5aaf8f59548ec4df49f681
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-10-29 17:29:21 +0200

    source control and sqlserver

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,3 +55,19 @@ resource "azurerm_linux_web_app" "alwa" {
     always_on = false
   }
 }
+
+resource "azurerm_app_service_source_control" "sc" {
+  app_id                 = azurerm_linux_web_app.alwa.id
+  repo_url               = var.repo_URL
+  branch                 = "main"
+  use_manual_integration = true
+}
+
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_administrator_login_username
+  administrator_login_password = var.sql_administrator_password
+}
