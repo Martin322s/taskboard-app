4ea3d740fd22e4cba1e1878e6beb1e78e27809b8
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-10-29 17:28:39 +0200

    azure resource group

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,17 @@ terraform {
     key                  = "terraform.tfstate"
   }
 }
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_group_name}${random_integer.ri.result}"
+  location = var.resource_group_location
+}
