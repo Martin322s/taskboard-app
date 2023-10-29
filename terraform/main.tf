931d92e50027b0833a88cc06be412cc4ce91314e
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-10-29 17:29:37 +0200

    database and firewall

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,3 +71,20 @@ resource "azurerm_mssql_server" "sqlserver" {
   administrator_login          = var.sql_administrator_login_username
   administrator_login_password = var.sql_administrator_password
 }
+
+resource "azurerm_mssql_database" "database" {
+  name           = var.sql_database_name
+  server_id      = azurerm_mssql_server.sqlserver.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "S0"
+  zone_redundant = false
+}
+
+resource "azurerm_mssql_firewall_rule" "firewall" {
+  name             = var.firewall_rule_name
+  server_id        = azurerm_mssql_server.sqlserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
